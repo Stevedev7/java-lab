b3fee39185d983e9f2fa9c388f90a47698f134d3
Rogue Jonin <pintosteve@yahoo.com>
2019-11-13 12:14:43 +0530

    Copy File Content program

---
+++ FileContent/FileCopy.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class FileCopy {
+    public static void main(String[] args) {
+        try{
+            FileInputStream file1 = new FileInputStream("first.md");
+            FileInputStream file2 = new FileInputStream("second.md");
+            FileOutputStream result = new FileOutputStream("result.md");
+            int i;
+            while((i = file1.read()) != -1){
+                result.write(i);
+            }
+            file1.close();
+            while((i = file2.read()) != -1){
+                result.write(i);
+            }
+            file2.close();
+            result.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
